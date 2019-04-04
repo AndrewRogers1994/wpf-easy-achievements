d55fa7d293b797d1c9b5d39d99ac23709f7a43b3
Andrew Rogers <andrewRogers1994@outlook.com>
2019-04-05 00:02:48 +0100

    Initial commit. Not much here at the moment

---
+++ TestApp/TestApp/Controls/BasicDialog.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Animation;
+
+namespace TestApp.Controls
+{
+    public partial class BasicDialog : UserControl
+    {
+        private bool acheivementVisible = false;
+        private const double animationTime = 0.5;
+
+        public BasicDialog()
+        {
+            InitializeComponent();
+        }
+
+        public void ToggleAcheivement()
+        {
+            if(acheivementVisible)
+            {
+                FadeOut();
+            }
+            else
+            {
+                FadeIn();
+            }
+        }
+
+        public async void ShowAcheivement(int time, Action AnimationCompleteCallback = null)
+        {
+            FadeIn();
+            await Task.Delay(TimeSpan.FromSeconds(animationTime + time));
+            FadeOut();
+            await Task.Delay(TimeSpan.FromSeconds(animationTime));
+            AnimationCompleteCallback?.Invoke();
+        }
+
+
+        private void FadeOut()
+        {
+            DoubleAnimation fadeOutAnimation = new DoubleAnimation
+            {
+                From = 1,
+                To = 0,
+                Duration = TimeSpan.FromSeconds(animationTime)
+            };
+
+            Storyboard.SetTarget(fadeOutAnimation, AcheivementWindow);
+            Storyboard.SetTargetProperty(fadeOutAnimation, new PropertyPath(UserControl.OpacityProperty));
+            Storyboard fadeOutStoryboard = new Storyboard();
+            fadeOutStoryboard.Children.Add(fadeOutAnimation);
+            fadeOutStoryboard.Completed += FadeOutStoryboard_Completed;
+            fadeOutStoryboard.Begin();
+            
+        }
+
+        private void FadeIn()
+        {
+            DoubleAnimation fadeInAnimation = new DoubleAnimation
+            {
+                From = 0,
+                To = 1,
+                Duration = TimeSpan.FromSeconds(animationTime)
+            };
+
+            Storyboard.SetTarget(fadeInAnimation, AcheivementWindow);
+            Storyboard.SetTargetProperty(fadeInAnimation, new PropertyPath(UserControl.OpacityProperty));
+            Storyboard fadeInStoryboard = new Storyboard();
+            fadeInStoryboard.Children.Add(fadeInAnimation);
+            fadeInStoryboard.Completed += FadeInStoryboard_Completed;
+            fadeInStoryboard.Begin();
+        }
+
+        private void FadeOutStoryboard_Completed(object sender, EventArgs e)
+        {
+            acheivementVisible = false;
+        }
+
+        private void FadeInStoryboard_Completed(object sender, EventArgs e)
+        {
+            acheivementVisible = true;
+        }
+
+    }
+}
---
+++ TestApp/TestApp/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace TestApp
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void FadeOutButton_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            testAcheivement.ShowAcheivement(2, new System.Action(() =>
+            {
+                MessageBox.Show("Animation Complete", "The Animation has completed");
+            }));
+        }
+    }
+}
